20859481864e06d86b0abcc6cd21fd475b128037
Diegolopez-ing <Diegolopez344@gmail.com>
2020-06-17 22:38:32 -0500

    Revisado por el profe por primera vez

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+
+//import Main from './components/Main';
+import Registro from './components/Registro';
+import Foot from './components/Foot';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="container">
+      <div className="col-md-12">
+        <div className="row">
+
+          <Foot />
+          
+        </div>
+
+        <div className="row">
+          <Registro />
+        </div>
+      </div>
+
+
+
+    </div>
+
+
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
